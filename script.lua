268503577579e2b8a9e3608975bdecf3973ad915
Contentgreg <atanasdenkov93@gmail.com>
2025-12-06 22:31:27 +0200

    Add morfOS script

--- script.lua
+++ script.lua
@@ -1,90 +1,97 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
-   Name = "Atanas' Exploiting Hub (OPEN SOURCE)",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "French fry loader",
-   LoadingSubtitle = "by Atanas",
-   ShowText = "Hub", -- for mobile users to unhide rayfield, change if you'd like
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+    Name = "Atanas' Exploiting Hub (OPEN SOURCE)",
+                                     Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+LoadingTitle = "French fry loader",
+LoadingSubtitle = "by Atanas",
+ShowText = "Hub", -- for mobile users to unhide rayfield, change if you'd like
+Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
-   ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
+ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
 
-   DisableRayfieldPrompts = true,
-   DisableBuildWarnings = true, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+DisableRayfieldPrompts = true,
+DisableBuildWarnings = true, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
 
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "AEH",
-   },
+ConfigurationSaving = {
+    Enabled = true,
+    FolderName = nil, -- Create a custom folder for your hub/game
+    FileName = "AEH",
+},
 
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true, -- Set this to false to make them join the discord every time they load it up
-   },
+Discord = {
+    Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+    Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+    RememberJoins = true, -- Set this to false to make them join the discord every time they load it up
+},
 
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+KeySystem = false, -- Set this to true to use our key system
+KeySettings = {
+    Title = "Untitled",
+    Subtitle = "Key System",
+    Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+    FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+    SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+    GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+    Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+}
 })
 
 local Tab = Window:CreateTab("Universal", "pin") -- Title, Image
 
 local Button = Tab:CreateButton({
-   Name = "Infinite Yield",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
-   end,
+    Name = "Infinite Yield",
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+    end,
 })
 
 local Button = Tab:CreateButton({
-   Name = "Cmd",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/lxte/cmd/main/main.lua"))()
-   end,
+    Name = "Cmd",
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lxte/cmd/main/main.lua"))()
+    end,
 })
 
 local Button = Tab:CreateButton({
-   Name = "BetterBypasser",
-   Callback = function()
-   loadstring(game:HttpGet("https://github.com/Synergy-Networks/products/raw/main/BetterBypasser/loader.lua"))()
-   end,
+    Name = "BetterBypasser",
+    Callback = function()
+    loadstring(game:HttpGet("https://github.com/Synergy-Networks/products/raw/main/BetterBypasser/loader.lua"))()
+    end,
+})
+
+local Button = Tab:CreateButton({
+    Name = "morfOS (NOT MACOS!!!)",
+    Callback = function()
+    loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-morfOS-Universal-46903"))()
+    end,
 })
 
 local Tab = Window:CreateTab("Game Specific", "gamepad-2") -- Title, Image
 
 local Button = Tab:CreateButton({
-   Name = "mshax (DOORS)",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/TheHunterSolo1/Scripts/refs/heads/main/Protected_2809220311826785.lua.txt"))()
-   end,
+    Name = "mshax (DOORS)",
+                                Callback = function()
+                                loadstring(game:HttpGet("https://raw.githubusercontent.com/TheHunterSolo1/Scripts/refs/heads/main/Protected_2809220311826785.lua.txt"))()
+                                end,
 })
 
 local Button = Tab:CreateButton({
-   Name = "Syla Hub (Total Roblox Drama)",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/SylaHub/Syla-Hub/refs/heads/main/Loader"))()
-   end,
+    Name = "Syla Hub (Total Roblox Drama)",
+                                Callback = function()
+                                loadstring(game:HttpGet("https://raw.githubusercontent.com/SylaHub/Syla-Hub/refs/heads/main/Loader"))()
+                                end,
 })
 
 local Button = Tab:CreateButton({
-   Name = "Moonlight Bottle (DOORS)",
-   Callback = function()
-   loadstring(game:HttpGet('https://gist.githubusercontent.com/IdkMyNameLoll/04d7dd5e02688624b958b8c2604b924c/raw/9e86b34249f44ed2dd433176e67daaf3db30cde8/MoonBottle'))()
-   end,
+    Name = "Moonlight Bottle (DOORS)",
+                                Callback = function()
+                                loadstring(game:HttpGet('https://gist.githubusercontent.com/IdkMyNameLoll/04d7dd5e02688624b958b8c2604b924c/raw/9e86b34249f44ed2dd433176e67daaf3db30cde8/MoonBottle'))()
+                                end,
 })
 
 local Button = Tab:CreateButton({
-   Name = "c00lkidd Seek (DOORS)",
-   Callback = function()
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/IdkMyNameLoll/PublicScripts/refs/heads/main/c00lkiddSeek"))()
-   end,
+    Name = "c00lkidd Seek (DOORS)",
+                                Callback = function()
+                                loadstring(game:HttpGet("https://raw.githubusercontent.com/IdkMyNameLoll/PublicScripts/refs/heads/main/c00lkiddSeek"))()
+                                end,
 })
